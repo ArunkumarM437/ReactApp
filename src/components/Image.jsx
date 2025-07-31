8fc70cdfca32f6728a752e4a24ffdeec3e0c25b0
Arunkumar-M174 <arunprince580@gmail.com>
2025-07-31 23:14:02 +0530

    added fevicon and condiotional rendering

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,15 +1,27 @@
 function RengokuCard({
-  image = "src/assets/213014.gif",
+  image = "src/assets/images/213014.gif",
   imageDes = "Rengoku - Flame Hashira",
-  quote = "No Matter What"
+  quote = "No Matter What",
+  show = false
 }) {
-  return (
-    <div className="card">
-      <img src={image} alt={imageDes} />
-      <p>{imageDes}</p>
-      <p>{quote}</p>
-    </div>
-  );
+  if (show == true) {
+    return (
+      <div className="card">
+        <img src={image} alt={imageDes} />
+        <p>{imageDes}</p>
+        <p>{quote}</p>
+      </div>
+    );
+  }else{
+    return(
+       <div className="card">
+        <img src="src\assets\images\akaza.gif" alt="Akaza" />
+        <p>"Orini Nara Nai"</p>
+        <p>"Rengoku Not Available"</p>
+      </div>
+    );
+  }
+
 }
 
 export default RengokuCard;
